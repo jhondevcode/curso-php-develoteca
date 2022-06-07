54118a5c0d15e098d3c7cb9977bcea35b4e1d52b
jhondevcode <jhondev.code@gmail.com>
2022-06-06 23:48:35 -0500

    echo, print_r, print

---
+++ src/ejercicio01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP | ejercicio01</title>
+  </head>
+  <body>
+    <?php
+      /*for ($i = 1; $i <= 6; $i++) {
+        echo "<h$i>Hola mundo</h$i>";
+      }*/
+      echo "Hola develoteca";
+      print_r("Hola develoteca");
+      print("Hola develoteca");
+      echo "\n";
+    ?>
+  </body>
+</html>
