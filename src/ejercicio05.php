daf4ed88aa245eb549ab3488bb6caeb9b23b0609
jhondevcode <jhondev.code@gmail.com>
2022-06-07 00:15:50 -0500

    Tipos de datos

---
+++ src/ejercicio05.php
@@ -0,0 +1,12 @@
+<?php
+
+  $edad = 32;           // Entero
+
+  $nombre = "Oscar";    // String
+
+  $altura = 1.4;        // Flotante o decimal
+
+  $programador=null;    // Valor nulo
+
+  echo "El nombre es ".$nombre.", la edad es ".$edad.", la altura es ".$altura.", el puesto ".$programador;
+?>
